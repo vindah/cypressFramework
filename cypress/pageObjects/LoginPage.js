5e22169b076e3701d305eb313745a9e0bc2ba494
vindah <patwikeyo2@gmail.com>
2022-11-05 02:46:50 +0100

    updated the locators for elements on login page

--- cypress/pageObjects/LoginPage.js
+++ cypress/pageObjects/LoginPage.js
@@ -1,14 +1,22 @@
 class LoginPage{
     get usernameField(){
-        return cy.get
+        return cy.get('[data-test="username"]');
     }
 
     get passwordField(){
-        return cy.get
+        return cy.get('[data-test="password"]');
     }
 
     get loginBtn(){
-        return cy.get
+        return cy.get('[data-test="login-button"]');
+    }
+
+    get loginLogo(){
+        return cy.get('.login_logo');
+    }
+
+    get botColumn(){
+        return cy.get('.bot_column');
     }
 }
 export default LoginPage;
