f007cc1ccb2cba4c268f42181cf83b7fda053fc7
vindah <patwikeyo2@gmail.com>
2022-11-05 02:31:44 +0100

    updated page object for elements and fixture file

---
+++ cypress/pageObjects/LoginPage.js
@@ -0,0 +1,14 @@
+class LoginPage{
+    get usernameField(){
+        return cy.get
+    }
+
+    get passwordField(){
+        return cy.get
+    }
+
+    get loginBtn(){
+        return cy.get
+    }
+}
+export default LoginPage;
